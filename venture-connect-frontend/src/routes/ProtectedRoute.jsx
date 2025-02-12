322714015ee253e03ce41f5a43654b1d63e5c3cd
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-02-12 23:59:26 +0530

    feat: created ProtectedRoute for authenticated user can access the thing

---
+++ venture-connect-frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+  let checked = false;
+  if (checked) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
--- venture-connect-frontend/src/routes/index.jsx
+++ venture-connect-frontend/src/routes/index.jsx
@@ -1,23 +1,35 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-import Landing from "../pages/Landing";
-import MainLayout from "../Layouts/MainLayout";
-import Signin from "../pages/auth/Signin";
-import Signup from "../pages/auth/Signup";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Landing from '../pages/Landing';
+import MainLayout from '../Layouts/MainLayout';
+import Signin from '../pages/auth/Signin';
+import Signup from '../pages/auth/Signup';
+import UserLayout from '../Layouts/UserLayout';
+import ProtectedRoute from './ProtectedRoute';
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/">
-            <Route element={<MainLayout />}>
-                <Route index element={<Landing />} />
-                <Route path="signin" element={<Signin />} />
-                <Route path="signup" element={<Signup />} />
-            </Route>
+  createRoutesFromElements(
+    <Route path="/">
+      <Route element={<MainLayout />}>
+        <Route index element={<Landing />} />
+        <Route path="signin" element={<Signin />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<UserLayout />}>
+          <Route path="/startups-hub" element={<Landing />} />
         </Route>
-    )
+      </Route>
+    </Route>,
+  ),
 );
 
 const AppRouters = () => {
-    return <RouterProvider router={router} />;
-}
+  return <RouterProvider router={router} />;
+};
 
 export default AppRouters;
