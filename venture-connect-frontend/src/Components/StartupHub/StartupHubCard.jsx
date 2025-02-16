82bcb89b2c8c0f240ab941b4a28e144b44e4591e
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-02-16 22:06:27 +0530

    enhance UI with additional details and icons

--- venture-connect-frontend/src/Components/StartupHub/StartupHubCard.jsx
+++ venture-connect-frontend/src/Components/StartupHub/StartupHubCard.jsx
@@ -1,39 +1,67 @@
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark, faBuilding } from '@fortawesome/free-regular-svg-icons';
+import {
+  faAngleRight,
+  faDollar,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import DefaultUser from '../../assets/images/default-user.png';
+import signupImage from '../../assets/images/signupSIdeImage.png';
+import { IoLocationOutline } from 'react-icons/io5';
 
 const StartupHubCard = () => {
   return (
-    <div className="bg-white max-w-sm md:max-w-md w-full p-5 rounded-2xl shadow-md mx-auto space-y-4">
-      <div className="flex justify-between items-center">
-        <img
-          src={DefaultUser}
-          alt="startup-image"
-          className="w-12 h-12 rounded-full object-cover"
-        />
-        <button className="px-3 py-1 text-sm font-medium border border-purple-500 text-purple-500 rounded-md hover:bg-purple-100 transition cursor-pointer">
-          Seed
+    <div className="bg-white max-w-sm md:max-w-md w-full xl:p-5 p-3 rounded-2xl shadow-md mx-auto space-y-4">
+      <div className="flex justify-between items-start lg:items-center flex-row">
+        <div className="flex items-start lg:items-center gap-3 flex-col lg:flex-row">
+          <img
+            src={signupImage}
+            alt="startup-image"
+            className="w-16 h-16 rounded-md object-cover"
+          />
+          <div>
+            <h3 className="text-2xl font-semibold">Relovent</h3>
+            <p className="text-gray-600 text-base flex items-center gap-1 mt-1">
+              <span>
+                <IoLocationOutline size="18" />
+              </span>
+              Mumbai, India
+            </p>
+          </div>
+        </div>
+        <button className="px-3 py-1 hover:cursor-pointer rounded-md hover:bg-gray-200 transition">
+          <FontAwesomeIcon
+            icon={faBookmark}
+            className="text-gray-600"
+            size="lg"
+          />
         </button>
       </div>
 
-      <div>
-        <h3 className="text-lg font-semibold">Relovent</h3>
-        <p className="text-gray-600 text-sm flex items-center gap-2">
-          <span className="font-medium">Tech</span> • Mumbai, India
-        </p>
-      </div>
-
-      <p className="text-gray-500 text-lg font-normal">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-        necessitatibus quia ad fugiat minima sequi!
+      <p className="text-gray-500 text-base font-normal">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, magni!
       </p>
+      <div className="flex items-center gap-3 text-base">
+        <div className="bg-sky-50 rounded-md text-blue-600 px-2 py-1 inline-block">
+          <p>AI</p>
+        </div>
+      </div>
 
-      <div className="flex items-center justify-between mt-4">
-        <button className="bg-blue-500 text-white px-4 py-2 text-sm rounded-md shadow-md hover:bg-blue-600 transition cursor-pointer">
-          Learn More
+      <div className="flex items-center text-base justify-between text-gray-500">
+        <p>
+          <FontAwesomeIcon icon={faDollar} /> $5.2M
+        </p>
+        <button>
+          <FontAwesomeIcon icon={faUsers} /> 20-25
         </button>
-        <button className="border border-gray-300 px-3 py-1 hover:cursor-pointer rounded-md hover:bg-gray-200 transition">
-          <FontAwesomeIcon icon={faBookmark} className="text-gray-600" />
+      </div>
+
+      <hr className="text-gray-200/90" />
+      <div className="flex items-center text-base justify-between mt-4">
+        <p className="text-gray-500">
+          <FontAwesomeIcon icon={faBuilding} /> Seed Stage
+        </p>
+        <button className="text-blue-500 hover:text-blue-600 transition cursor-pointer">
+          Learn More <FontAwesomeIcon icon={faAngleRight} />
         </button>
       </div>
     </div>
