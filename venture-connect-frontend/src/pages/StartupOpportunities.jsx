9b38450420721b832c37f83b74974fa8d673d847
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-02-18 11:08:42 +0530

    feat: add StartupOpportunities page with search, filter and stats

---
+++ venture-connect-frontend/src/pages/StartupOpportunities.jsx
@@ -0,0 +1,60 @@
+import { FilterOutlined, SearchOutlined } from '@ant-design/icons';
+import { Button, Input } from 'antd';
+import OppStartupCard from '../components/Opportunities/OppStartupCard';
+
+const StartupOpportunities = () => {
+  const cards = Array(4).fill(0);
+  return (
+    <>
+      <div className="space-y-2">
+        <h1 className="text-4xl font-bold">
+          Find Your Next Pitch{' '}
+          <span className="text-blue-500">Opportunity</span>
+        </h1>
+        <p className="text-lg text-gray-500 font-normal">
+          Connect with investors and accelerators looking for promising startups
+          like yours
+        </p>
+      </div>
+
+      <div className="flex flex-wrap items-center gap-4 my-5">
+        <Input
+          placeholder="Search Opportunities..."
+          prefix={<SearchOutlined className="text-gray-500 px-1 pe-1.5" />}
+          allowClear
+          className="flex-1 min-w-md rounded-lg h-10 bg-gray-100"
+        />
+        <Button
+          icon={<FilterOutlined className="text-gray-700" />}
+          className="flex-0 rounded-full h-10 bg-white border-gray-300 font-medium"
+          size="large"
+        >
+          Filters
+        </Button>
+      </div>
+
+      <div className="flex gap-x-6 items-center text-gray-600 break-words flex-wrap">
+        <p className="text-lg">
+          <span className="text-blue-500 text-xl font-bold">200+ </span>Active
+          Opportunities
+        </p>
+        <p className="text-lg">
+          <span className="text-blue-500 text-xl font-bold">50K+ </span>Total
+          Investment
+        </p>
+        <p className="text-lg">
+          <span className="text-blue-500 text-xl font-bold">1000+ </span>
+          Startups Funded
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-8">
+        {cards.map((_, index) => (
+          <OppStartupCard key={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default StartupOpportunities;
