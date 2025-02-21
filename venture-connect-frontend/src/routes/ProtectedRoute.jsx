c0e27408c38bd958d739082ea77b755219f38160
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-02-21 16:09:10 +0530

    feat: redirect startup user to complete startup profile page if not completed

--- venture-connect-frontend/src/routes/ProtectedRoute.jsx
+++ venture-connect-frontend/src/routes/ProtectedRoute.jsx
@@ -1,9 +1,14 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  let isAuthenticated = false;
-  if (isAuthenticated) {
+  let isAuthenticated = true;
+  if (!isAuthenticated) {
     return <Navigate to="/signin" replace />;
+  }
+
+  let isProfileCompleted = false;
+  if (isProfileCompleted) {
+    return <Navigate to="/complete-startup-profile" replace />;
   }
 
   return <Outlet />;
