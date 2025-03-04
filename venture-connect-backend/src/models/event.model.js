e20d0407e91d9a6fab2fb3ade51a314845f319a9
pitroda-meet <mpitroda459@rku.ac.in>
2025-03-04 09:45:38 +0530

    event model design

---
+++ venture-connect-backend/src/models/event.model.js
@@ -0,0 +1,88 @@
+import sequelize from '../config/database';
+import { DataTypes, Model } from 'sequelize';
+
+class Event extends Model {
+  getKeyHighlights() {
+    return this.keyHighlights ? this.keyhighlights.split('\n') : [];
+  }
+
+  setKeyHighlights(highlightsArray) {
+    this.keyhighlights = highlightsArray.join('\n');
+  }
+
+  getWhoShouldAttend() {
+    return this.whoShouldAttend ? this.whoShouldAttend.split('\n') : [];
+  }
+  setWhoShouldAttend(WhoShouldAttendArray) {
+    this.whoShouldAttend = WhoShouldAttendArray.join('\n');
+  }
+}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subtitle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capacity: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        min: 1,
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    keyhighlights: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    whoShouldAttend: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    timeFrom: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    timeTill: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    event_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'event',
+    modelName: 'Event',
+    timestamps: true,
+  },
+);
