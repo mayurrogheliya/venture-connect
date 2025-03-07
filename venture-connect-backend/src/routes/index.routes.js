be5c82ee27f30a467db1c629426ded4602da7d70
pitroda-meet <mpitroda459@rku.ac.in>
2025-03-07 18:17:49 +0530

    Opportunity routes create

--- venture-connect-backend/src/routes/index.routes.js
+++ venture-connect-backend/src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import startupRoutes from './startup.routes.js';
 import eventRoutes from './event.routes.js';
+import opportRoutes from './opportunity.routes.js';
 const router = Router();
 
 router.use('/startup', startupRoutes);
 router.use('/event', eventRoutes);
+router.use('/opportunity', opportRoutes);
 
 export default router;
