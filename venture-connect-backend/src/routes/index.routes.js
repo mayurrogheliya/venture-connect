bdf36b0f316bad3d30496904d65293b4f3d26b26
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-07 17:46:47 +0530

    feat: add investor routes for CRUD operations on investor profiles

--- venture-connect-backend/src/routes/index.routes.js
+++ venture-connect-backend/src/routes/index.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import startupRoutes from './startup.routes.js';
 import eventRoutes from './event.routes.js';
+import investorRoutes from './investor.routes.js';
+
 const router = Router();
 
 router.use('/startup', startupRoutes);
 router.use('/event', eventRoutes);
+router.use('/investor', investorRoutes);
 
 export default router;
---
+++ venture-connect-backend/src/routes/investor.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import * as investoreController from '../controllers/investor.controller.js';
+import { investorUploadMiddleware } from '../middlewares/uploadMiddleware.js';
+
+const router = Router();
+
+router.post(
+  '/createInvestor',
+  investorUploadMiddleware,
+  investoreController.createInvestorProfile,
+);
+router.get('/getInvestors', investoreController.getInvestors);
+router.get('/getInvestor/:investorId', investoreController.getInvestor);
+router.put(
+  '/updateInvestor/:investorId',
+  investorUploadMiddleware,
+  investoreController.updateInvestorProfile,
+);
+router.delete(
+  '/deleteInvestor/:investorId',
+  investoreController.deleteInvestorProfile,
+);
+
+export default router;
