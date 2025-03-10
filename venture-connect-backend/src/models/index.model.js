75c296d0b7a5a781d1af2037feed53bf5337a7ea
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-11 00:06:49 +0530

    ref: update associations to link startup-related models via Startup entity

--- venture-connect-backend/src/models/index.model.js
+++ venture-connect-backend/src/models/index.model.js
@@ -1,20 +1,24 @@
 import InvestorBasicInfo from './InvestorBasicInfo.model.js';
 import InvestorInvestmentDetails from './InvestorInvestmentDetails.model.js';
 import PreviousInvestment from './previousInvestment.mode.js';
+import Startup from './startup.model.js';
 import StartupBasicInfo from './startupBasicInfo.model.js';
 import StartupMetrics from './startupMetrics.model.js';
 import StartupTeam from './startupTeam.model.js';
 import StartupTeamMember from './StartupTeamMember.model.js';
 import User from './user.model.js';
 
-User.hasOne(StartupBasicInfo, { foreignKey: 'userId', as: 'basicInfo' });
-StartupBasicInfo.belongsTo(User, { foreignKey: 'userId' });
+User.hasOne(Startup, { foreignKey: 'userId', as: 'startup' });
+Startup.belongsTo(User, { foreignKey: 'userId' });
 
-User.hasOne(StartupMetrics, { foreignKey: 'userId', as: 'metrics' });
-StartupMetrics.belongsTo(User, { foreignKey: 'userId' });
+Startup.hasOne(StartupBasicInfo, { foreignKey: 'startupId', as: 'basicInfo' });
+StartupBasicInfo.belongsTo(Startup, { foreignKey: 'startupId' });
 
-User.hasOne(StartupTeam, { foreignKey: 'userId', as: 'team' });
-StartupTeam.belongsTo(User, { foreignKey: 'userId' });
+Startup.hasOne(StartupMetrics, { foreignKey: 'startupId', as: 'metrics' });
+StartupMetrics.belongsTo(Startup, { foreignKey: 'startupId' });
+
+Startup.hasOne(StartupTeam, { foreignKey: 'startupId', as: 'team' });
+StartupTeam.belongsTo(Startup, { foreignKey: 'startupId' });
 
 StartupTeam.hasMany(StartupTeamMember, {
   foreignKey: 'teamId',
@@ -42,6 +46,7 @@ PreviousInvestment.belongsTo(User, { foreignKey: 'investorId' });
 
 export {
   User,
+  Startup,
   StartupBasicInfo,
   StartupMetrics,
   StartupTeam,
