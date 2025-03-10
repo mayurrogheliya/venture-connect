b5b272ce4eb6ba78940bc524a508f006270502dc
pitroda-meet <mpitroda459@rku.ac.in>
2025-03-10 20:56:02 +0530

    feat: set up event attendance routes

---
+++ venture-connect-backend/src/routes/eventAttend.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as EventAttendController from '../controllers/eventAttend.controller.js';
+
+const router = express.Router();
+
+router.post('/registerevent', EventAttendController.registerForEvent);
+router.get('/getallattendees', EventAttendController.getAllAttendees);
+router.get('/getattendeebyid/:id', EventAttendController.getAttendeeById);
+router.put('/updateattendee/:id', EventAttendController.updateAttendee);
+router.delete('/deleteattendee/:id', EventAttendController.deleteAttendee);
+
+export default router;
