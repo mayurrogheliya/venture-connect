d5583e873bddfeb396466d04cbeeb413c695bd64
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-11 18:59:46 +0530

    add CORS middleware to enable cross-origin requests

--- venture-connect-backend/src/app.js
+++ venture-connect-backend/src/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import routes from './routes/index.routes.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100kb' }));
