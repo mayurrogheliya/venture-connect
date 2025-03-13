9ff0537ae87e602647c8ca0424dd8ec68299032b
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-14 00:00:54 +0530

    feat: add Axios interceptors for request and response handling

--- venture-connect-frontend/src/api/axios.js
+++ venture-connect-frontend/src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useUserStore } from '../store/useUserStore';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -19,5 +20,34 @@ export const multipartRequest = axios.create({
   },
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    console.log(error);
+
+    const { logout } = useUserStore.getState();
+    if (
+      error.response &&
+      (error.response.status === 401 || error.response.status == 403)
+    ) {
+      logout();
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
