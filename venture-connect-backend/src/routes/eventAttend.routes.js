626656b86e587bd4bb113ff23e42cf4276f558cf
pitroda-meet <mpitroda459@rku.ac.in>
2025-03-17 11:26:56 +0530

    id add in get all attend in routes

--- venture-connect-backend/src/routes/eventAttend.routes.js
+++ venture-connect-backend/src/routes/eventAttend.routes.js
@@ -4,7 +4,7 @@ import * as EventAttendController from '../controllers/eventAttend.controller.js
 const router = express.Router();
 
 router.post('/registerevent', EventAttendController.registerForEvent);
-router.get('/getallattendees', EventAttendController.getAllAttendees);
+router.get('/getallattendees/:id', EventAttendController.getAllAttendees);
 router.get('/getattendeebyid/:id', EventAttendController.getAttendeeById);
 router.put('/updateattendee/:id', EventAttendController.updateAttendee);
 router.delete('/deleteattendee/:id', EventAttendController.deleteAttendee);
