730fa6cfab69307d8b31ce1b40e1559beed5fd96
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-17 13:52:46 +0530

    feat: enhance user store with user retrieval and improved login/logout handling

--- venture-connect-frontend/src/store/useUserStore.js
+++ venture-connect-frontend/src/store/useUserStore.js
@@ -1,5 +1,7 @@
 import { create } from 'zustand';
-export const useUserStore = create((set) => {
+import { usersAPI } from '../api/endpoints/users';
+import { message } from 'antd';
+export const useUserStore = create((set, get) => {
   const savedToken = localStorage.getItem('accessToken');
   const isAuthenticated = savedToken !== null;
   return {
@@ -7,14 +9,26 @@ export const useUserStore = create((set) => {
     loading: false,
     accessToken: savedToken,
     isAuthenticated,
+    setUser: (user) => set({ user }),
     setLoading: (loading) => set({ loading }),
     login: (user, token) => {
       localStorage.setItem('accessToken', token);
+      localStorage.setItem('userId', user.id);
       set({ user, isAuthenticated: true, accessToken: token });
     },
     logout: () => {
       localStorage.removeItem('accessToken');
+      localStorage.removeItem('userId');
       set({ user: null, isAuthenticated: false, accessToken: null });
+    },
+    getUserById: async (userId) => {
+      try {
+        const response = await usersAPI.getUser(userId);
+        get().setUser(response?.data);
+      } catch (error) {
+        console.log(error);
+        message.error(error?.response?.data?.message);
+      }
     },
   };
 });
