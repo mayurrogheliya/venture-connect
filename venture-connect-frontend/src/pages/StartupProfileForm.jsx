3ac1b5e2088f2297b6a2c80d474831f6843d291e
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-19 18:51:30 +0530

    feat: implement form submission for startup profile creation

--- venture-connect-frontend/src/pages/StartupProfileForm.jsx
+++ venture-connect-frontend/src/pages/StartupProfileForm.jsx
@@ -3,9 +3,15 @@ import SPBasicInfo from '../components/StartupProfileSteps/SPBasicInfo';
 import { useState } from 'react';
 import SPMatrices from '../components/StartupProfileSteps/SPMetrices';
 import SPTeam from '../components/StartupProfileSteps/SPTeam';
+import { useStartupProfileStore } from '../store/useStartupProfileStore';
+import { startupAPI } from '../api/endpoints/startup';
+import { useNavigate } from 'react-router-dom';
 
 const StartupProfileForm = () => {
   const [form] = Form.useForm();
+  const userId = localStorage.getItem('userId');
+  const { setLoading } = useStartupProfileStore();
+  const navigate = useNavigate();
 
   const steps = [
     {
@@ -35,6 +41,77 @@ const StartupProfileForm = () => {
     title: item.title,
   }));
 
+  const handleSubmit = async () => {
+    try {
+      await form.validateFields();
+      setLoading(true);
+
+      const formValues = form.getFieldsValue(true);
+      const formData = new FormData();
+
+      formData.append('userId', userId);
+      formData.append('basicInfo', JSON.stringify(formValues.basicInfo));
+      formData.append('metrics', JSON.stringify(formValues.metrics));
+      formData.append('team', JSON.stringify(formValues.team));
+      formData.append(
+        'teamMembers',
+        JSON.stringify(formValues.teamMembers || []),
+      );
+
+      console.log('form values ', formValues);
+
+      if (formValues.basicInfo?.startupLogo?.[0]?.originFileObj) {
+        formData.append(
+          'startupLogo',
+          formValues.basicInfo.startupLogo[0].originFileObj,
+        );
+      }
+
+      if (formValues.team?.founderImage?.[0]?.originFileObj) {
+        formData.append(
+          'founderImage',
+          formValues.team.founderImage[0].originFileObj,
+        );
+      }
+
+      if (formValues.teamMembers) {
+        formValues.teamMembers.forEach((member, index) => {
+          if (member.profile_image?.[0]?.originFileObj) {
+            formData.append(
+              'teamMembersImages',
+              member.profile_image[0].originFileObj,
+            );
+          }
+        });
+      }
+
+      for (let pair of formData.entries()) {
+        console.log(
+          pair[0] +
+            ': ' +
+            (pair[1] instanceof File
+              ? `File: ${pair[1].name}, size: ${pair[1].size}`
+              : pair[1]),
+        );
+      }
+
+      const response = await startupAPI.createStartupProfile(formData);
+      console.log('response', response);
+      if (response.data.success) {
+        message.success(
+          response?.data?.message || 'Startup Profile Created Successfully',
+        );
+        navigate('/startups-hub');
+      } else {
+        message.error(response?.data?.message || 'Failed to create profile');
+      }
+    } catch (error) {
+      message.error(error?.response?.data?.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <div className="bg-neutral-50 md:pt-10 md:pb-5 md:px-20 pt-5 pb-3 px-10 border-b border-gray-200">
@@ -62,14 +139,7 @@ const StartupProfileForm = () => {
             </Button>
           )}
           {current === steps.length - 1 && (
-            <Button
-              type="primary"
-              onClick={async () => {
-                await form.validateFields();
-                message.success('Processing complete!');
-                console.log(form.getFieldsValue(true));
-              }}
-            >
+            <Button type="primary" onClick={handleSubmit}>
               Submit
             </Button>
           )}
