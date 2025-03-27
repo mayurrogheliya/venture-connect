de564aa5ba8055534a43c5bf7ec1eb923b1cfdf1
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-27 21:13:57 +0530

    feat: implement startup profile fetching and loading state in StartupUserTable component

--- venture-connect-frontend/src/Components/StartupUserMain/StartupUserTable.jsx
+++ venture-connect-frontend/src/Components/StartupUserMain/StartupUserTable.jsx
@@ -3,10 +3,20 @@ import {
   EditOutlined,
   SearchOutlined,
 } from '@ant-design/icons';
-import { Button, Input, Space, Table, Tooltip } from 'antd';
-import { useState } from 'react';
+import { Button, Flex, Input, Space, Spin, Table, Tooltip } from 'antd';
+import { useEffect, useState } from 'react';
+import { useStartupProfileStore } from '../../store/useStartupProfileStore';
+import { formatAmount } from '../../utils/formatUtils';
 
 const StartupUserTable = () => {
+  const {
+    getAllStartupProfiles,
+    startupAllProfile,
+    setEditingStartupProfile,
+    setLoading,
+    loading,
+    setMode,
+  } = useStartupProfileStore();
   const [searchText, setSearchText] = useState('');
   const [pageSize, setPageSize] = useState(5);
 
@@ -26,38 +36,31 @@ const StartupUserTable = () => {
     );
   };
 
-  const investorData = [
-    {
-      startup_name: 'Startup A',
-      industry: 'Tech',
-      team_size: 5,
-      valuation: '$100,000',
-    },
-    {
-      startup_name: 'Startup B',
-      industry: 'Finance',
-      team_size: 10,
-      valuation: '$500,000',
-    },
-    {
-      startup_name: 'Startup C',
-      industry: 'Tech',
-      team_size: 3,
-      valuation: '$150,000',
-    },
-    {
-      startup_name: 'Startup D',
-      industry: 'Finance',
-      team_size: 7,
-      valuation: '$200,000',
-    },
-    {
-      startup_name: 'Startup E',
-      industry: 'Tech',
-      team_size: 4,
-      valuation: '$120,000',
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        await getAllStartupProfiles();
+      } catch (error) {
+        console.error('Error fetching startup profiles:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [getAllStartupProfiles]);
+
+  const startupData =
+    Array.isArray(startupAllProfile) &&
+    startupAllProfile?.map((item) => ({
+      id: item?.id,
+      startup_name: item?.startup?.basicInfo?.startup_name,
+      industry: item?.startup?.basicInfo?.industry,
+      team_size: formatAmount(item?.startup?.basicInfo?.team_size),
+      current_valuation: formatAmount(
+        item?.startup?.metrics?.current_valuation,
+      ),
+    }));
 
   const columns = [
     {
@@ -76,32 +79,45 @@ const StartupUserTable = () => {
       title: 'Team Size',
       dataIndex: 'team_size',
       key: 'team_size',
-      render: (text) => highlightText(text, searchText),
     },
     {
       title: 'Valuation',
-      dataIndex: 'valuation',
-      key: 'valuation',
+      dataIndex: 'current_valuation',
+      key: 'current_valuation',
     },
     {
       title: 'Actions',
       key: 'actions',
       render: (_, record) => (
         <Space size={0}>
-          <Tooltip title="Edit Investor">
+          <Tooltip title="Edit Startup">
             <Button
               type="text"
               icon={<EditOutlined />}
               style={{ color: '#2ecc71' }}
-              onClick={() => console.log('Edit Investor')}
+              onClick={() => {
+                const fullProfile = startupAllProfile.find(
+                  (item) => item?.id === record.id,
+                );
+                if (fullProfile) {
+                  setEditingStartupProfile({
+                    ...fullProfile.startup,
+                    userId: fullProfile.id,
+                  });
+                  setMode('form');
+                  console.log('Full Startup Profile:', fullProfile.startup);
+                } else {
+                  console.error('Startup profile not found');
+                }
+              }}
             />
           </Tooltip>
-          <Tooltip title="Delete Investor">
+          <Tooltip title="Delete Startup">
             <Button
               type="text"
               icon={<DeleteOutlined />}
               style={{ color: '#e74c3c' }}
-              onClick={() => console.log('Delete Investor')}
+              onClick={() => console.log('Delete Startup')}
             />
           </Tooltip>
         </Space>
@@ -109,11 +125,16 @@ const StartupUserTable = () => {
     },
   ];
 
-  const filteredData = investorData.filter((record) =>
-    ['startup_name', 'industry', 'team_size'].some((key) =>
-      record[key]?.toString().toLowerCase().includes(searchText),
-    ),
-  );
+  const filteredData =
+    Array.isArray(startupAllProfile) &&
+    startupData?.filter((record) =>
+      ['startup_name', 'industry'].some((key) =>
+        record[key]
+          ?.toString()
+          .toLowerCase()
+          .includes(searchText.toLowerCase()),
+      ),
+    );
 
   return (
     <>
@@ -124,21 +145,27 @@ const StartupUserTable = () => {
         onChange={handleSearch}
         value={searchText}
       />
-      <Table
-        style={{ overflowX: 'auto' }}
-        columns={columns}
-        dataSource={filteredData}
-        rowKey="id"
-        bordered={true}
-        pagination={{
-          pageSize: pageSize,
-          showSizeChanger: true,
-          showQuickJumper: true,
-          showTotal: (total) => `Total ${total} investors`,
-          pageSizeOptions: ['5', '10', '20'],
-          onShowSizeChange: (_, size) => setPageSize(size),
-        }}
-      />
+      {loading ? (
+        <Flex gap="middle" vertical>
+          <Spin tip="Loading..." size="large" />
+        </Flex>
+      ) : (
+        <Table
+          style={{ overflowX: 'auto' }}
+          columns={columns}
+          dataSource={filteredData}
+          rowKey="id"
+          bordered={true}
+          pagination={{
+            pageSize: pageSize,
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: (total) => `Total ${total} Startups`,
+            pageSizeOptions: ['5', '10', '20'],
+            onShowSizeChange: (_, size) => setPageSize(size),
+          }}
+        />
+      )}
     </>
   );
 };
