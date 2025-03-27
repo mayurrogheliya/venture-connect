6273a31aacc959f2ff63320090200acbf8588a70
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-27 21:10:52 +0530

    feat: add getter methods to parse decimal values in startupMetrics

--- venture-connect-backend/src/models/startupMetrics.model.js
+++ venture-connect-backend/src/models/startupMetrics.model.js
@@ -13,46 +13,79 @@ StartupMetrics.init(
     mrr: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('mrr'));
+      },
     },
     yoy: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('yoy'));
+      },
     },
     total_funding: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('total_funding'));
+      },
     },
     current_valuation: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('current_valuation'));
+      },
     },
     investment_amount: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('investment_amount'));
+      },
     },
     equity_offered: {
       type: DataTypes.DECIMAL(5, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('equity_offered'));
+      },
     },
     cac: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('cac'));
+      },
     },
     ltv: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('ltv'));
+      },
     },
     monthly_burn_rate: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('monthly_burn_rate'));
+      },
     },
     annualRevenue: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('annualRevenue'));
+      },
     },
     profitMargin: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      get() {
+        return parseFloat(this.getDataValue('profitMargin'));
+      },
     },
   },
   {
