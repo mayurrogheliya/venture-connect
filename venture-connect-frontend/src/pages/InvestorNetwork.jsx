35c2ffda201c6a2d7fa4cc7ecfbca83cc7fbb506
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-03-31 17:40:09 +0530

    feat(investor-network): enhance investor cards with dynamic data and loading state

--- venture-connect-frontend/src/pages/InvestorNetwork.jsx
+++ venture-connect-frontend/src/pages/InvestorNetwork.jsx
@@ -1,44 +1,37 @@
-import React from 'react';
-import { Input, Button, Typography } from 'antd';
+import { useEffect } from 'react';
+import { Input, Button, Flex, Spin, Empty } from 'antd';
 import { FilterOutlined, SearchOutlined } from '@ant-design/icons';
 import InvestorNetworkCard from '../Components/InvestorNetwork/InvestorNetworkCard';
-
-const { Title, Text } = Typography;
-
-const investors = [
-  {
-    name: 'Dhruv Burada',
-    role: 'VC',
-    location: 'GUJ, IN',
-    sector: 'Health Care',
-  },
-  {
-    name: 'Mayur Rogheliya',
-    role: 'Angel Investor',
-    location: 'GUJ, IN',
-    sector: 'Education',
-  },
-  {
-    name: 'Meet Pitroda',
-    role: 'Private Equity',
-    location: 'MH, IN',
-    sector: 'Manufacturing',
-  },
-];
+import { useInvestorProfileStore } from '../store/useInvestorProfileStore';
+import { useUserStore } from '../store/useUserStore';
 
 const InvestorNetwork = () => {
+  const { getAllInvestorProfiles, investorAllProfile, setLoading, loading } =
+    useInvestorProfileStore();
+  const { userId } = useUserStore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      await getAllInvestorProfiles(userId);
+      setLoading(false);
+    };
+    fetchData();
+  }, [getAllInvestorProfiles]);
+
+  console.log('investor all profile', investorAllProfile);
+
   return (
     <div className="space-y-2">
-    <h1 className="text-4xl font-bold">
-      Discover Right{' '}
-      <span className="text-blue-500">Investors</span>
-    </h1>
-    <p className="text-lg text-gray-500 font-normal">
-    Discover investors who align with your vision and take your startup to the next level.
-    </p>
+      <h1 className="text-4xl font-bold">
+        Discover Right <span className="text-blue-500">Investors</span>
+      </h1>
+      <p className="text-lg text-gray-500 font-normal">
+        Discover investors who align with your vision and take your startup to
+        the next level.
+      </p>
 
-
-    <div className="flex flex-wrap items-center gap-4 my-5">
+      <div className="flex flex-wrap items-center gap-4 my-5">
         <Input
           placeholder="Search Investors..."
           prefix={<SearchOutlined className="text-gray-500 px-1 pe-1.5" />}
@@ -69,12 +62,30 @@ const InvestorNetwork = () => {
         </p>
       </div>
 
-      {/* Investor Cards with Proper Spacing */}
-      <div className="mt-8 grid gap-6">
-        {investors.map((investor, index) => (
-          <InvestorNetworkCard key={index} {...investor} />
-        ))}
-      </div>
+      {loading ? (
+        <Flex gap="middle" vertical>
+          <Spin tip="Loading..." size="large" />
+        </Flex>
+      ) : (
+        <div className="mt-8 grid gap-6">
+          {Array.isArray(investorAllProfile) &&
+          investorAllProfile.length > 0 ? (
+            investorAllProfile.map((items, index) => (
+              <InvestorNetworkCard
+                key={index}
+                investor={items?.investor}
+                userId={items?.id}
+              />
+            ))
+          ) : (
+            <Empty
+              description="No investors found"
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+              className="flex flex-col items-center justify-center py-12"
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 };
