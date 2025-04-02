8c06ccc2842b55b73fe0be9f3446e25b4db60b33
pitroda-meet <mpitroda459@rku.ac.in>
2025-04-03 00:02:39 +0530

    feat: bookmark functionality integration

--- venture-connect-frontend/src/Components/StartupHub/StartupHubCard.jsx
+++ venture-connect-frontend/src/Components/StartupHub/StartupHubCard.jsx
@@ -10,15 +10,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IoLocationOutline } from 'react-icons/io5';
 import { formatAmount } from '../../utils/formatUtils';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import bookmarkApi from '../../api/endpoints/bookmark';
+import { toast } from 'react-toastify';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const StartupHubCard = ({ startup, userId }) => {
   const { basicInfo, metrics } = startup || {};
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
   const handleLearnMore = () => {
     navigate(`/startup-profile/${userId}`);
   };
 
-  const isBookmarked = false;
+  const handleBookmarkToggle = async () => {
+    setIsLoading(true);
+    try {
+      if (!isBookmarked) {
+        const response = await bookmarkApi.createBookmark(startup.id);
+        setIsBookmarked(true);
+        if (response.data.message) toast.success(response.data.message);
+        navigate(`/bookmarks`);
+      } else {
+        toast.warning(response.data.message);
+      }
+    } catch (error) {
+      console.error('Failed to add bookmark:', error.message);
+      if (error.response?.data?.message) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error('An error occurred while adding the bookmark.');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="bg-white w-full max-w-sm p-4 rounded-2xl shadow-md space-y-4 mx-auto">
       <div className="flex justify-between items-start">
@@ -40,13 +69,22 @@ const StartupHubCard = ({ startup, userId }) => {
         {/* Bookmark / Delete Icon */}
         <button
           className="px-3 py-1 hover:bg-gray-200 rounded-md"
-          onClick={() => isBookmarked}
+          onClick={handleBookmarkToggle}
+          disabled={isLoading}
         >
-          <FontAwesomeIcon
-            icon={isBookmarked ? faTrash : faBookmark}
-            className={`text-gray-500`}
-            size="lg"
-          />
+          {isLoading ? (
+            <FontAwesomeIcon
+              icon={faSpinner}
+              className="text-gray-500 animate-spin"
+              size="lg"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={isBookmarked ? faTrash : faBookmark}
+              className="text-gray-500"
+              size="lg"
+            />
+          )}
         </button>
       </div>
 
