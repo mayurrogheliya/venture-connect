cf41a7d71204c36320b66ccc1064e1530d6c2940
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-04-17 10:33:00 +0530

    feat: display investor profile image when editing profile

--- venture-connect-frontend/src/Layouts/AdminLayout.jsx
+++ venture-connect-frontend/src/Layouts/AdminLayout.jsx
@@ -37,10 +37,6 @@ const AdminLayout = () => {
 
   const items = [
     {
-      key: '1',
-      label: <NavLink to="/change-password">Change Password</NavLink>,
-    },
-    {
       key: '2',
       label: (
         <button
